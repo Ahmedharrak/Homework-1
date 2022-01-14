0e81ebdadacc6a063e19144cf90e68bf8a2f8c3c
Your Name <you@example.com>
2022-01-14 17:57:33 +0100

    version 1 de projet

--- Homework 1/main.js
+++
@@ -1,6 +0,0 @@
-﻿function change() {
-    var str = document.getElementById("TextBox1").value;
-    var res = str.toUpperCase();
-    return document.getElementById("userInput").textContent = res;
-
-}
